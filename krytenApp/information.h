60d7a5d54e37952495db487e9b8a22ae17627bac
Andrew Starritt <starritt@netspace.net.au>
2021-02-12 11:37:33 +1100

    Update version to 2.2.3

--- krytenApp/information.h
+++ krytenApp/information.h
@@ -3,7 +3,7 @@
  * $DateTime: 2015/11/01 15:48:18 $
  * Last checked in by: $Author: andrew $
  *
- * Copyright (C) 2011-2019  Andrew C. Starritt
+ * Copyright (C) 2011-2021  Andrew C. Starritt
  * See kryten.c for details.
  *
  */
@@ -11,7 +11,7 @@
 #ifndef INFORMATION_H_
 #define INFORMATION_H_
 
-#define KRYTEN_VERSION    "2.2.2"
+#define KRYTEN_VERSION    "2.2.3"
 #define BUILD_DATETIME    __DATE__ " " __TIME__
 
 /* All these functions print information only.
